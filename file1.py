5c94a1313f8e069da3b1a9db0f9c06387ff2591a
jyothsna <merugujyoshna123@gmail.com>
2024-03-08 17:33:51 -0800

    apply changes

--- file1.py
+++ file1.py
@@ -1,2 +1,11 @@
 Hi 
 This is my first file
+<<<<<<< Updated upstream
+||||||| Stash base
+Hello world
+welcome to devops class
+=======
+Hello world
+welcome to devops class
+hello everyone
+>>>>>>> Stashed changes
